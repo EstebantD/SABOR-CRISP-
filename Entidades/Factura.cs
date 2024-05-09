95ef2f94ae4aa7b09a4696228c8477e418db5e31
TvDD <edaviddaza@unicesar.edu.co>
2024-05-08 21:15:40 -0500

    Agregar archivos de proyecto.

---
+++ Entidades/Factura.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Entidades
+{
+
+    public class Factura : Productos
+    {
+        public Factura() { }
+        public DateTime Fecha_Compra { get; set; }
+
+        public Factura(string documento, DateTime fecha_Compra, string nombre_Productos, int precio_Total)
+        {
+            Documento = documento;
+            Fecha_Compra = fecha_Compra;
+            Nombre_Producto = nombre_Productos;
+            Precio_Total = precio_Total;
+        }
+    }
+
+}
---
+++ Logica/Servicio_Factura.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logica
+{
+    public class ServicioFactura
+    {
+        Datos.RepositorioFactura repositorioFactura;
+        public ServicioFactura(string connectionString)
+        {
+             = new Datos.RepositorioFactura(connectionString);
+        }
+        public void Insertar(Factura factura)
+        {
+            repositorioFactura.Insert(factura);
+        }
+        public string Total_Ingresos()
+        {
+            int total = 0;
+            foreach (var item in GetAll())
+            {
+                total += item.Precio_Total;
+            }
+            return total.ToString();
+        }
+        public DataTable GetAllProductos_Cliente(string documento)
+        {
+            return repositorioFactura.GetAllProductos_Cliente(documento);
+        }
+        public List<Factura> GetAll()
+        {
+            return repositorioFactura.GetAll();
+        }
+        public DataTable GetFactura()
+        {
+            return repositorioFactura.GetAllTabla();
+        }
+    }
+}
